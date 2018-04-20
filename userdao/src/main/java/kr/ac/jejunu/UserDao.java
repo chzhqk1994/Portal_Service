3802554d9ccded171e834f3572aeaa8a600db1a0
hyunwoo <chzhqk1994@gmail.com>
2018-04-21 00:56:35 +0900

    Lambda

--- userdao/src/main/java/kr/ac/jejunu/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/UserDao.java
@@ -1,8 +1,5 @@
 package kr.ac.jejunu;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import javax.sql.DataSource;
 import java.sql.SQLException;
 
 public class UserDao {
@@ -13,26 +10,30 @@ public class UserDao {
     }
 
     public User get(int id) throws SQLException, ClassNotFoundException {
-        StatementStrategy statementStrategy = new GetUserStatementStrategy(id);
-        return jdbcContext.jdbcContextForGet(statementStrategy);
+        String sql = "SELECT * FROM test WHERE id = ?";
+        Object[] params = new Object[]{id};
+        return jdbcContext.queryForObject(sql, params);
     }
 
 
     public Integer insert(User user) throws SQLException, ClassNotFoundException {
-        StatementStrategy statementStrategy = new InserstUserStatementStrategy(user);
-        return jdbcContext.jdbcContextForInsert(statementStrategy);
+        String sql = "INSERT INTO test(name, password) VALUES (?, ?)";
+        Object[] params = new Object[]{user.getName(), user.getPassword()};
+        return jdbcContext.insert(sql, params);
     }
 
 
     public void update(User user) throws SQLException, ClassNotFoundException {
-        StatementStrategy statementStrategy = new UpdateUserStatementStrategy(user);
-        jdbcContext.jdbcContextForUpdate(statementStrategy);
+        String sql = "UPDATE test SET name = ?, password = ? WHERE id = ?";
+        Object[] params = new Object[]{user.getName(), user.getPassword(), user.getId()};
+        jdbcContext.update(sql, params);
 
     }
 
     public void delete(Integer id) throws SQLException, ClassNotFoundException {
-        StatementStrategy statementStrategy = new DeleteUserStatementStrategy(id);
-        jdbcContext.jdbcContextForUpdate(statementStrategy);
+        String sql = "DELETE FROM test WHERE id = ?";
+        Object[] params = new Object[]{id};
+        jdbcContext.update(sql, params);
     }
 
 }
